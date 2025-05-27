69266383c1fddeb491451d57491731586c4767a1
faizansafwan <faizansafwan075@gmail.com>
2025-05-27 21:38:39 +0530

    initialized the settings page

---
+++ app/(tabs)/settings.tsx
@@ -0,0 +1,14 @@
+import { ScrollView, Text, View } from "react-native";
+
+export default function Settings() {
+
+    return(
+        <ScrollView>
+            <View>
+                <View>
+                    <Text> Settings page</Text>
+                </View>
+            </View>
+        </ScrollView>
+    )
+}
