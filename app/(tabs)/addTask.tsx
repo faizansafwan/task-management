8ed79bc39980305b5f6378679b67b5788fd1f121
faizansafwan <faizansafwan075@gmail.com>
2025-05-27 21:30:15 +0530

    initialized the add task

---
+++ app/(tabs)/addTask.tsx
@@ -0,0 +1,13 @@
+import { ScrollView, Text, View } from "react-native";
+
+
+export default function AddTask() {
+
+    return (
+        <ScrollView>
+            <View>
+                <Text> Add Task</Text>
+            </View>
+        </ScrollView>
+    )
+}
