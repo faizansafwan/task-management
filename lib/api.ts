9b5cc4f03aa5d74e157bd782e51e0734fe82b8de
faizansafwan <faizansafwan075@gmail.com>
2025-05-28 23:11:02 +0530

    logic for update api created

--- lib/api.ts
+++ lib/api.ts
@@ -6,4 +6,20 @@ export async function getTaskList() {
       throw new Error('Failed to fetch todos');
     }
     return response.json();
+}
+
+export async function updateTask(id: string, updatedFields: Partial<{ title: string; description: string; status: string; dueDate: string }>) {
+  const response = await fetch(`${BASE_URL}/todo/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(updatedFields),
+  });
+
+  if (!response.ok) {
+    throw new Error('Failed to update task');
   }
+
+  return response.json();
+}
