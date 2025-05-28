7e4d9430310274ac62feb6dfa15875a4cb803628
faizansafwan <faizansafwan075@gmail.com>
2025-05-28 19:23:03 +0530

    theme color added

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,12 +3,13 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
+
 const tintColorLight = '#0a7ea4';
 const tintColorDark = '#fff';
 
 export const Colors = {
   light: {
-    text: '#11181C',
+    text: '#000',
     background: '#fff',
     tint: tintColorLight,
     icon: '#687076',
@@ -16,11 +17,19 @@ export const Colors = {
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
+    text: '#fff',
+    background: '#000',
     tint: tintColorDark,
     icon: '#9BA1A6',
     tabIconDefault: '#9BA1A6',
     tabIconSelected: tintColorDark,
   },
+
+  primary: {
+    Background: '#626F47',
+  },
+  
+  secondary: {
+    Background: '#A4B465',
+  }
 };
