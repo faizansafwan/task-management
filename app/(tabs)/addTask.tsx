d0c1f75b7112506ca9b98abf4c1b19e302ca5e80
faizansafwan <faizansafwan075@gmail.com>
2025-05-28 21:56:28 +0530

    fetch the todo data

--- app/(tabs)/addTask.tsx
+++ app/(tabs)/addTask.tsx
@@ -1,12 +1,14 @@
-import { ScrollView, Text, View } from "react-native";
+import { ThemedText } from "@/components/ThemedText";
+import { Colors } from "@/constants/Colors";
+import { ScrollView, View } from "react-native";
 
 
 export default function AddTask() {
 
     return (
         <ScrollView>
-            <View>
-                <Text> Add Task</Text>
+            <View style={{ margin: 20}}>
+                <ThemedText lightColor={Colors.light.text} darkColor={Colors.dark.text}> Add Task</ThemedText>
             </View>
         </ScrollView>
     )
